feef3c8c7525b7937409e9802b84350d72dd3dc8
nitreojs <n1trojscoding@mail.ru>
2019-11-21 17:02:59 +0300

    Remove useless typings

--- packages/puregram/src/api.d.ts
+++
@@ -1,9 +0,0 @@
-import Interfaces from '../typings/interfaces';
-
-declare class API {
-  public call(method: string, params: object): Promise<object>;
-  
-  public getMe(): Promise<Interfaces.IUser>;
-}
-
-export = API;

398f25b1ea01e88b1c8d7b5cc1204ae5662b2eb8
nitrojs <n1trojscoding@mail.ru>
2019-11-23 14:16:54 +0300

    Add keyboard builder examples

--- docs/examples/keyboards.js
+++ docs/examples/keyboards.js
@@ -1,11 +1,19 @@
-let { Telegram, Keyboard, InlineKeyboard } = require('puregram');
+let {
+  Telegram,
+  Keyboard,
+  KeyboardBuilder,
+  InlineKeyboard,
+  InlineKeyboardBuilder,
+} = require('puregram');
 
 let telegram = new Telegram({
   token: process.env.TOKEN,
 });
 
 telegram.updates.setHearFallbackHandler(
-  context => context.send('Enter either /keyboard or /inline.'),
+  context => context.send(
+    'Enter either /keyboard, /keyboardbuilder, /inline or /inlinebuilder.',
+  ),
 );
 
 telegram.updates.hear('/keyboard', (context) => {
@@ -26,6 +34,21 @@ telegram.updates.hear('/keyboard', (context) => {
         Keyboard.textButton('buttons!'),
       ],
     ]).resize(),
+  });
+});
+
+telegram.updates.hear('/keyboardbuilder', (context) => {
+  return context.send('There\'s your keyboard!', {
+    reply_markup: new KeyboardBuilder()
+      .textButton('Button')
+      .row()
+      .textButton('Another button')
+      .textButton('But there\'s more!')
+      .row()
+      .textButton('So')
+      .textButton('many')
+      .textButton('buttons!')
+      .resize(),
   });
 });
 
@@ -66,8 +89,25 @@ telegram.updates.hear('/inline', (context) => {
   });
 });
 
+telegram.updates.hear('/inlinebuilder', (context) => {
+  return context.send('There\'s your keyboard!', {
+    reply_markup: new InlineKeyboardBuilder()
+      .textButton({ text: 'Inline button!' })
+      .row()
+      .textButton({ text: 'And there\'s more!' })
+      .textButton({
+        text: 'Payload button',
+        payload: { payload: true },
+      })
+      .row()
+      .textButton({ text: 'This' })
+      .textButton({ text: 'is' })
+      .textButton({ text: 'epic!' }),
+  });
+});
+
 telegram.updates.on('callback_query', (context) => {
-  return context.answerCallbackQuery('You just clicked the payload button!');
+  return context.answerCallbackQuery('You just clicked the inline button!');
 });
 
 telegram.updates.startPolling();

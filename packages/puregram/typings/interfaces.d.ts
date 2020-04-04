222c5193ea6907b8f0263ce657df29af6ff3168b
nitrojs <n1trojscoding@mail.ru>
2020-04-04 23:36:28 +0300

    Fix typings due to latest commit

--- packages/puregram/typings/interfaces.d.ts
+++ packages/puregram/typings/interfaces.d.ts
@@ -3835,5 +3835,49 @@ export interface IInlineQueryResultVoice {
 }
 
 export interface IDice {
+  /**
+   * Message is a dice with random value from `1` to `6`
+   */
   value: number;
+}
+
+/**
+ * This object represents a game.
+ * 
+ * Use BotFather to create and edit games, their short names will act as unique identifiers.
+ */
+export interface IGame {
+  /**
+   * Title of the game
+   */
+  title: string;
+
+  /**
+   * Description of the game
+   */
+  description: string;
+
+  /**
+   * Photo that will be displayed in the game message in chats.
+   */
+  photo: Array<IPhotoSize>;
+
+  /**
+   * Brief description of the game or high scores included in the game message.
+   * Can be automatically edited to include current high scores for the game
+   * when the bot calls `setGameScore`, or manually edited using `editMessageText`.
+   * 
+   * 0-4096 characters.
+   */
+  text?: string;
+
+  /**
+   * Special entities that appear in text, such as usernames, URLs, bot commands, etc.
+   */
+  textEntities?: Array<IMessageEntity>;
+
+  /**
+   * Animation that will be displayed in the game message in chats
+   */
+  animation?: IAnimation;
 }

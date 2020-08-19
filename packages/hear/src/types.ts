7fab198973f87a0a62049ef23392f169cae1b3c4
nitreojs <n1trojscoding@mail.ru>
2020-08-19 12:25:33 +0300

    fix(hear): object properties should be partial

--- packages/hear/src/types.ts
+++ packages/hear/src/types.ts
@@ -4,9 +4,10 @@ export type HearFunctionCondition<T, V> = (value: V, context: T) => boolean;
 
 export type HearCondition<T, V> = HearFunctionCondition<T, V> | RegExp | string;
 
-export type HearObjectCondition<T extends Record<string, any>> = {
-  [P in keyof T]: AllowArray<HearCondition<T, T[P]>>;
-};
+export type HearObjectCondition<T extends Record<string, any>> =
+  Record<string, AllowArray<HearCondition<T, any>>> & {
+    [P in keyof T]?: AllowArray<HearCondition<T, T[P]>>;
+  };
 
 export type HearConditions<T> = (
   AllowArray<HearCondition<T, string | undefined>>

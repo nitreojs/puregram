32bbe493e122ce60c4e1c298bd0676efe1f5b18b
nitrojs <discordrexx@mail.ru>
2020-08-26 16:44:02 +0300

    feat: import src/common/attachments

--- packages/puregram/src/common/attachments/index.ts
+++ packages/puregram/src/common/attachments/index.ts
@@ -2,6 +2,7 @@ export * from './animation';
 export * from './attachment';
 export * from './audio';
 export * from './document';
+export * from './file-attachment';
 export * from './photo';
 export * from './sticker';
 export * from './video';
--- packages/puregram/src/index.ts
+++ packages/puregram/src/index.ts
@@ -3,8 +3,7 @@ export * from './contexts';
 export * from './enums';
 export * from './common/reply-markup';
 export * from './common/keyboards';
+export * from './common/attachments';
 export * from './errors';
 
 export { Composer } from './common/structures/composer';
-export { Attachment } from './common/attachments/attachment';
-export { FileAttachment } from './common/attachments/file-attachment';

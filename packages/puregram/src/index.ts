7d3895de3c2a81d5561e28691408a21b9804e711
nitrojs <discordrexx@mail.ru>
2020-11-04 14:12:41 +0300

    chore: export common/structures

--- packages/puregram/src/index.ts
+++ packages/puregram/src/index.ts
@@ -4,6 +4,5 @@ export * from './enums';
 export * from './common/reply-markup';
 export * from './common/keyboards';
 export * from './common/attachments';
+export * from './common/structures';
 export * from './errors';
-
-export { Composer } from './common/structures/composer';

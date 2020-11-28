4ae18bba74125b883843b757adbab0fefbdefb11
nitrojs <discordrexx@mail.ru>
2020-11-28 22:18:28 +0300

    chore(test): small changes in contexts.test.ts

--- packages/puregram/test/contexts.test.ts
+++ packages/puregram/test/contexts.test.ts
@@ -7,19 +7,19 @@ const telegram: Telegram = new Telegram();
 
 describe('Contexts', (): void => {
   describe('Context', (): void => {
-    describe('#<Context>.is', (): void => {
-      const context: Context = new Context(telegram, 'message');
+    const context: Context = new Context(telegram, 'message');
 
+    describe('#<Context>.is', (): void => {
       it('should return true when the context coincides with the input', (): void => {
-        expect(context.is('message')).toEqual(true);
-        expect(context.is(['message'])).toEqual(true);
-        expect(context.is(['message', 'edited_message'])).toEqual(true);
+        expect(context.is('message')).toBe(true);
+        expect(context.is(['message'])).toBe(true);
+        expect(context.is(['message', 'edited_message'])).toBe(true);
       });
 
       it('should return false when the context does not coincide with the input', (): void => {
-        expect(context.is('callback_query')).toEqual(false);
-        expect(context.is(['callback_query'])).toEqual(false);
-        expect(context.is(['callback_query', 'channel_post'])).toEqual(false);
+        expect(context.is('callback_query')).toBe(false);
+        expect(context.is(['callback_query'])).toBe(false);
+        expect(context.is(['callback_query', 'channel_post'])).toBe(false);
       });
     });
   });

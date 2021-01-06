6b6896ffa9980df42c6e3345d019198aa4797560
nitrojs <discordrexx@mail.ru>
2021-01-06 13:02:25 +0300

    chore(examples): add editing example

---
+++ docs/examples/editing.js
@@ -0,0 +1,106 @@
+import { Telegram, InlineKeyboard } from 'puregram';
+import { HearManager } from '@puregram/hear';
+
+const telegram = new Telegram({
+  token: process.env.TOKEN
+});
+
+const hearManager = new HearManager();
+
+telegram.updates.on('message', hearManager.middleware);
+
+// Editing text in a text message
+hearManager.hear('/text', async (context) => {
+  const sentMessage = await context.send('Some *message* to edit.', {
+    parse_mode: 'Markdown'
+  });
+
+  setTimeout(
+    () => sentMessage.editMessageText('Some _edited message text_.', {
+      parse_mode: 'Markdown'
+    }),
+    1000
+  );
+});
+
+// Editing a caption of some media attachment
+hearManager.hear('/caption', async (context) => {
+  const sentMessage = await context.sendPhoto('./photo.jpg', {
+    caption: 'Some *photo* with _caption_',
+    parse_mode: 'Markdown'
+  });
+
+  setTimeout(
+    () => sentMessage.editMessageCaption('Edited *photo* _caption_ goes here', {
+      parse_mode: 'Markdown'
+    }),
+    1000
+  );
+});
+
+// Editing the media attachment
+hearManager.hear('/media', async (context) => {
+  const sentMessage = await context.sendDocument('./file1.txt', {
+    caption: 'Document #1'
+  });
+
+  setTimeout(
+    () => sentMessage.editMessageMedia({
+      type: 'document',
+      media: './file2.txt',
+      caption: 'Document #2'
+    }),
+    1000
+  );
+});
+
+// Editing the inline keyboard
+hearManager.hear('/markup', async (context) => {
+  const firstKeyboard = InlineKeyboard.keyboard([
+    [
+      InlineKeyboard.textButton({
+        text: 'Some inline button',
+        payload: { dummy: true }
+      })
+    ]
+  ]);
+
+  const secondKeyboard = InlineKeyboard.keyboard([
+    [
+      InlineKeyboard.textButton({
+        text: 'Button #1',
+        payload: { dummy: true }
+      }),
+
+      InlineKeyboard.textButton({
+        text: 'Button #2',
+        payload: { dummy: true }
+      })
+    ],
+
+    [
+      InlineKeyboard.textButton({
+        text: 'Button #3',
+        payload: { dummy: true }
+      })
+    ]
+  ]);
+
+  const sentMessage = await context.send('Some message with an inline keyboard.', {
+    reply_markup: firstKeyboard
+  });
+
+  setTimeout(
+    () => sentMessage.editMessageReplyMarkup(secondKeyboard),
+    1000
+  );
+});
+
+// Triggered when no other hears triggered
+hearManager.onFallback(
+  (context) => context.send('Command not found.')
+);
+
+telegram.updates.startPolling().then(
+  () => console.log(`Started polling @${telegram.bot.username}`)
+).catch(console.error);

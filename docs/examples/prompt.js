40ee1b9cf24edf2cef457982732b7794df27ec5f
nitrojs <discordrexx@mail.ru>
2021-06-22 19:57:32 +0300

    Add prompt example

---
+++ docs/examples/prompt.js
@@ -0,0 +1,24 @@
+import { Telegram } from 'puregram';
+import { PromptManager } from '@puregram/prompt';
+
+const telegram = new Telegram({
+  token: process.env.TOKEN
+});
+
+const manager = new PromptManager();
+
+telegram.updates.use(manager.middleware);
+
+telegram.updates.on('message', async (context) => {
+  let answer;
+
+  while (!answer || !answer.context.hasAttachments('photo')) {
+    answer = await context.prompt('*Send nudes* pls', { parse_mode: 'markdown' });
+  }
+
+  await context.send('Yooooo thanks for nudes cya');
+});
+
+telegram.updates.startPolling().then(
+  () => console.log(`Started polling @${telegram.bot.username}`)
+).catch(console.error);

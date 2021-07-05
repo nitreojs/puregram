ef1d62b955bbfba7e2c782b87590bbe82daefb1a
nitrojs <discordrexx@mail.ru>
2021-07-05 20:44:24 +0300

    fix startPolling() not returning any value -> .then() was not calling

--- packages/puregram/src/updates.ts
+++ packages/puregram/src/updates.ts
@@ -363,7 +363,7 @@ export class Updates {
     this.isStarted = true;
 
     try {
-      this.startFetchLoop(options);
+      return this.startFetchLoop(options);
     } catch (error) {
       this.isStarted = false;
 

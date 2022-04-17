64a4aa3ef736528ddb468769f170cdb1d2c6947f
starkow <nitrojs@mail.ru>
2022-04-17 09:50:53 +0300

    fix `UpdateName` being `string | number`

--- packages/puregram/src/types.ts
+++ packages/puregram/src/types.ts
@@ -4,7 +4,7 @@ import { ApiMethods } from './api-methods'
 export type AllowArray<T> = T | T[]
 
 export type MessageEventName = 'new_chat_members' | 'left_chat_member' | 'new_chat_title' | 'new_chat_photo' | 'delete_chat_photo' | 'group_chat_created' | 'supergroup_chat_created' | 'channel_chat_created' | 'migrate_to_chat_id' | 'migrate_from_chat_id' | 'pinned_message' | 'invoice' | 'successful_payment' | 'location' | 'message_auto_delete_timer_changed' | 'video_chat_scheduled' | 'video_chat_started' | 'video_chat_ended' | 'video_chat_participants_invited' | 'web_app_data' | 'chat_join_request'
-export type UpdateName = Exclude<keyof TelegramUpdate, 'update_id'> | MessageEventName
+export type UpdateName = Exclude<keyof Known<TelegramUpdate>, 'update_id'> | MessageEventName
 export type AttachmentType = 'animation' | 'audio' | 'document' | 'photo' | 'sticker' | 'video' | 'video_note' | 'voice'
 export type MediaAttachmentType = AttachmentType | 'media' | 'png_sticker' | 'tgs_sticker' | 'thumb'
 export type Constructor<T = {}> = new (...args: any[]) => T

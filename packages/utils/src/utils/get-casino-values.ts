685a2610ea828d11f1df89cc0b484b60974a88cb
starkow <nitrojs@mail.ru>
2022-06-11 16:27:31 +0300

    smol refactoring

--- packages/utils/src/utils/get-casino-values.ts
+++ packages/utils/src/utils/get-casino-values.ts
@@ -1,10 +1,21 @@
-type CasinoValue = 'bar' | 'grapes' | 'lemon' | 'seven'
+enum CasinoValue {
+  Bar = 'bar',
+  Grapes = 'grapes',
+  Lemon = 'lemon',
+  Seven = 'seven'
+}
+
 type SlotMachineValue = [CasinoValue, CasinoValue, CasinoValue]
 
-const casinoValues: CasinoValue[] = ['bar', 'grapes', 'lemon', 'seven']
+const casinoValues: CasinoValue[] = [
+  CasinoValue.Bar,
+  CasinoValue.Grapes,
+  CasinoValue.Lemon,
+  CasinoValue.Seven
+]
 
 /** Converts base-10 number to base-4 */
-const toBase4 = (number: number): number => {
+const toBase4 = (number: number) => {
   let result: number[] = []
 
   while (number > 0) {
@@ -19,15 +30,15 @@ const toBase4 = (number: number): number => {
 }
 
 /** Returns slot machine's values */
-export const getCasinoValues = (source: number | string): SlotMachineValue => {
-  const rawNumber: number = toBase4(
+export const getCasinoValues = (source: number | string) => {
+  const rawNumber = toBase4(
     typeof source === 'string'
       ? Number.parseInt(source)
       : source
   )
 
-  const number: string = String(rawNumber).padStart(3, '0')
-  let result: SlotMachineValue = [] as unknown as SlotMachineValue
+  const number = String(rawNumber).padStart(3, '0')
+  let result = []
 
   for (const char of number) {
     let int: number = Number.parseInt(char)

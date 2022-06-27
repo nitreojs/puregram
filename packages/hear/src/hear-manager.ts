58cfbca6acd146a1c5e2e121af10bbcaff8b0b9d
starkow <nitrojs@mail.ru>
2022-06-27 13:58:43 +0300

    add `HearManager.command` method
    - disallow `Context` as a generic, only `MessageContext` will do
    - wtf was that `this.hear(/^\/start/, ...)`?! removed it

--- packages/hear/src/hear-manager.ts
+++ packages/hear/src/hear-manager.ts
@@ -1,8 +1,7 @@
-import { Context, Composer } from 'puregram'
+import { Composer, MessageContext } from 'puregram'
 
 import {
   Middleware,
-  MiddlewareReturn,
   NextMiddleware,
   skipMiddleware
 } from 'middleware-io'
@@ -15,7 +14,7 @@ import {
   splitPath
 } from './helpers'
 
-export class HearManager<C extends Context> {
+export class HearManager<C extends MessageContext> {
   private composer = Composer.builder<C & ContextMatch>()
   private fallbackHandler: Middleware<C & ContextMatch> = skipMiddleware
   private composed!: Middleware<C & ContextMatch>
@@ -99,15 +98,14 @@ export class HearManager<C extends Context> {
     const needText = textCondition && !functionCondition
 
     this.composer.use(
-      (context: C & ContextMatch, next: NextMiddleware): MiddlewareReturn => {
-        // @ts-expect-error
-        const { text, caption } = context
-
-        if (needText && text === undefined && caption === undefined) {
+      (context: C & ContextMatch, next: NextMiddleware) => {
+        if (needText && !context.hasText && !context.hasCaption) {
           return next()
         }
 
-        const hasSome = conditions.some(condition => condition(text ?? caption, context))
+        const text = context.text ?? context.caption!
+
+        const hasSome = conditions.some(condition => condition(text, context))
 
         return hasSome
           ? handler(context, next)
@@ -120,9 +118,65 @@ export class HearManager<C extends Context> {
     return this
   }
 
+  command(command: string, handler: Middleware<C>) {
+    if (!command) {
+      throw new Error('command should not be empty')
+    }
+
+    if (typeof handler !== 'function') {
+      throw new TypeError('handler must be a function')
+    }
+
+    this.composer.use(
+      (context: C, next: NextMiddleware) => {
+        if (!context.hasText && !context.hasCaption) {
+          return next()
+        }
+
+        const text = context.text ?? context.caption!
+
+        const firstWord = text.split(/\s/)[0]
+
+        // INFO: starts with a '/' followed by a bunch of symbols (might have '@' and a bunch of symbols ending with 'bot')
+        // INFO: /foo_bar@username_bot
+        if (/^\/\w+(?:@\w+bot)?/.test(firstWord)) {
+          // INFO: 't' for 'text'
+          const [tCommandRaw, tUsername] = firstWord.split('@')
+
+          const hasUsername = tUsername !== undefined
+          const contextHasBotInfo = context.telegram.bot?.username !== undefined
+
+          // INFO: we have '@<username>' part but we don't have info about our bot
+          if (hasUsername && !contextHasBotInfo) {
+            return next()
+          }
+
+          // INFO: we have '@<username>' part but usernames do not match
+          if (hasUsername && tUsername.toLowerCase() !== context.telegram.bot.username!.toLowerCase()) {
+            return next()
+          }
+
+          // INFO: since command starts with '/' and we don't have that in [command] we need to slice that
+          const tCommand = tCommandRaw.slice(1)
+
+          if (tCommand !== command) {
+            return next()
+          }
+
+          return handler(context, next)
+        }
+      }
+    )
+
+    this.recompose()
+
+    return this
+  }
+
   /** A handler that is called when handlers are not found */
   onFallback(handler: Middleware<C>) {
     this.fallbackHandler = handler
+
     this.recompose()
 
     return this

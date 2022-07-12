7a7a41a731ae4129192c9240e8af280f5623a97e
starkow <nitrojs@mail.ru>
2022-07-12 17:47:52 +0300

    chore(commitlint): update config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  'scope-enum': [
+    'puregram',
+    'hear',
+    'prompt',
+    'scenes',
+    'session',
+    'utils',
+    'api',
+    'updates',
+    'eslint',
+    'commitlint'
+  ],
+  'scope-case': 'kebab-case'
 }

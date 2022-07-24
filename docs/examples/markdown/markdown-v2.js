02bf9b9b773d6fb73f777854ed500c6e8272680c
starkow <hello@starkow.dev>
2022-07-24 17:56:43 +0300

    chore(docs): add `MarkdownV2.build` usage example

--- docs/examples/markdown/markdown-v2.js
+++ docs/examples/markdown/markdown-v2.js
@@ -12,10 +12,14 @@ const telegram = new Telegram({
 telegram.updates.on('message', (context) => {
   const message = `a ${MarkdownV2.bold('message')} with some ${MarkdownV2.strikethrough('epic')} ${MarkdownV2.underline('markdown')}\\!`
   const anotherMessage = 'same *message* with ~epic~ __markdown__\\.'
+  const oneMoreMessage = MarkdownV2.build`
+    this is a ${MarkdownV2.bold('message')}! although, everything that is not ${MarkdownV2.italic('interpolated')} is escaped automatically.
+  `
 
   await Promise.all([
     context.send(message, { parse_mode: 'markdownv2' }), // <-- note how we are telling telegram
-    context.send(anotherMessage, { parse_mode: 'markdownv2' }) //     that there are some markdownv2 entities in the message
+    context.send(anotherMessage, { parse_mode: 'markdownv2' }), // that there are some markdownv2 entities in the message
+    context.send(oneMoreMessage, { parse_mode: 'markdownv2' })
   ])
 })
 

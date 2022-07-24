6175029aec7d625fd93c5a5edc475f69ccd73563
starkow <hello@starkow.dev>
2022-07-24 21:03:23 +0300

    feat(contexts): add LocationContext

---
+++ packages/puregram/src/contexts/location.ts
@@ -0,0 +1,59 @@
+import { inspectable } from 'inspectable'
+
+import * as Interfaces from '../generated/telegram-interfaces'
+import { Location } from '../common/structures'
+
+import { Telegram } from '../telegram'
+import { Message } from '../updates/'
+import { applyMixins } from '../utils/helpers'
+import { Constructor } from '../types/types'
+
+import { Context } from './context'
+import { NodeMixin, SendMixin, TargetMixin, CloneMixin } from './mixins'
+
+interface LocationContextOptions {
+  telegram: Telegram
+  update: Interfaces.TelegramUpdate
+  payload: Interfaces.TelegramMessage
+  updateId: number
+}
+
+class LocationContext extends Context {
+  payload: Interfaces.TelegramMessage
+
+  constructor (options: LocationContextOptions) {
+    super({
+      telegram: options.telegram,
+      updateType: 'left_chat_member',
+      updateId: options.updateId,
+      update: options.update
+    })
+
+    this.payload = options.payload
+  }
+
+  /** Location */
+  get location () {
+    return new Location(this.payload.location as Interfaces.TelegramLocation)
+  }
+}
+
+interface LocationContext extends Constructor<LocationContext>, Message, TargetMixin, SendMixin, NodeMixin, CloneMixin<LocationContext, LocationContextOptions> { }
+applyMixins(LocationContext, [Message, TargetMixin, SendMixin, NodeMixin, CloneMixin])
+
+inspectable(LocationContext, {
+  serialize (context) {
+    return {
+      id: context.id,
+      from: context.from,
+      senderId: context.senderId,
+      createdAt: context.createdAt,
+      chat: context.chat,
+      chatId: context.chatId,
+      chatType: context.chatType,
+      location: context.location
+    }
+  }
+})
+
+export { LocationContext }

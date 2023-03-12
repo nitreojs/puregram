ad542cd3bd4646e38a3f03b6a89b9b0f0584aa19
nitreojs <hello@starkow.dev>
2023-03-12 22:18:26 +0300

    chore(structures): add missing jsdoc descriptions

--- packages/puregram/src/common/structures/general-forum-topic-unhidden.ts
+++ packages/puregram/src/common/structures/general-forum-topic-unhidden.ts
@@ -4,7 +4,7 @@ import * as Interfaces from '../../generated/telegram-interfaces'
 
 import { Structure } from '../../types/interfaces'
 
-/** This object represents a service message about General forum topic hidden in the chat. Currently holds no information. */
+/** This object represents a service message about General forum topic unhidden in the chat. Currently holds no information. */
 export class GeneralForumTopicUnhidden implements Structure {
   constructor (public payload: Interfaces.TelegramGeneralForumTopicUnhidden) { }
 

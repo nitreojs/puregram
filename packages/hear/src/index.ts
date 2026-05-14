12cdeb42d580c650fa6e522c6d9a09f71ba6dae9
nitreojs <hello@starkow.dev>
2026-05-14 19:06:02 +0300

    chore: get rid of deprecated packages

--- packages/hear/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './types'
-
-export { HearManager } from './hear-manager'
--- packages/prompt/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './prompt-question'
-export * from './prompt-answer'
-export * from './prompt-manager'
-
-export { PromptContext } from './types'
